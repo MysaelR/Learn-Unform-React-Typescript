203a91125b646b83931119abeef7becc9c0c2e21
MysaelR <mysael.ti@gmail.com>
2022-02-09 16:02:18 -0300

    iniciando o projeto

---
+++ multiform/src/routes/router.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Route path="/" />
+        </BrowserRouter>
+    );
+};
+export default Router;
